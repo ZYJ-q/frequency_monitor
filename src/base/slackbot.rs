747dcbf155ffba1d7aefb9ae828c7cc61801b1a5
ZYJ-q <2979049853@qq.com>
2023-08-03 13:37:07 +0800

    p

--- src/base/slackbot.rs
+++ src/base/slackbot.rs
@@ -26,10 +26,9 @@ impl SlackHttpClient {
 
     pub async fn send_text(&self, sender: &str, content: &str) {
         let mut params: HashMap<String, Value> = HashMap::new();
-        params.insert(String::from("msgtype"), Value::from("text"));
-        let mut map: Map<String, Value> = Map::new();
-        map.insert(String::from("content"), Value::from(self.package_text(sender, content)));
-        params.insert(String::from("text"), Value::from(map));
+        // let mut map: Map<String, Value> = Map::new();
+        // map.insert(String::from("content"), Value::from(self.package_text(sender, content)));
+        params.insert(String::from("text"), Value::from(self.package_text(sender, content)));
         let mut headers = HeaderMap::new();
         headers.insert("Content-Type", "application/json".parse().unwrap());
         println!("POST {} {:?} {}", &self.slackbot, headers, &serde_json::to_string(&params).unwrap());
